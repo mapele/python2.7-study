3c8459a93d25ae53d6a8f45da02abf1c484784d8
mapele <278478117@qq.com>
2014-10-24 16:53:47 +0800

    sds

---
+++ files/Guitest.py
@@ -0,0 +1,19 @@
+import easygui
+
+#消息框
+user_response = easygui.msgbox("Hello world!")
+print(user_response)
+
+#按钮框
+flavor = easygui.buttonbox("What is you favorite icecream flavor?", choices = ['icecream1', 'ice2', 'ice3'])
+easygui.msgbox("You picked " + flavor)
+
+#选择框
+flavor = easygui.choicebox("What is you favorite icecream flavor?", choices = ['icecream1', 'ice2', 'ice3'])
+easygui.msgbox("You picked " + flavor)
+
+#输入框
+flavor = easygui.enterbox("What is you favorite icecream flavor?", default='auto')
+easygui.msgbox("You picked " + flavor)
+
+#整数框
